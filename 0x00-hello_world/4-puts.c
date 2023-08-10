7c217b3018215640f9076f3ffdea9e7ee6c9bd0c
JayJust0h <edwinjustus11@gmail.com>
2023-08-10 11:40:46 -0700

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
  * Main - Using the puts instead of printf command to output a function.
  * Return:0(Pass)
  */
-int main(void)
+int main(void);
 {
 puts("\"Programming is like building a multilingual puzzle");
-return 0;
+return (0);
 }
