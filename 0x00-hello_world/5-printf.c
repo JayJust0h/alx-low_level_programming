830ce17bf759853f341b63c4300107a51378a3ba
JayJust0h <edwinjustus11@gmail.com>
2023-08-10 12:23:58 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Using the printf command to output a line.
+ * Return:0 (pass)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
