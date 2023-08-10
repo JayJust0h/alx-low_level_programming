1e2a2659ed5516134640fc8da161f0c7f35aa1f0
JayJust0h <edwinjustus11@gmail.com>
2023-08-10 14:35:47 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - c program to print a line into the standard error file.
+ * Return: 1(Pass)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n,stdout");
+return (1);
+}
