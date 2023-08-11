6a25f57b060fa075998a6af9e5d169fcd83ecc8b
JayJust0h <edwinjustus11@gmail.com>
2023-08-11 12:52:50 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,27 +8,27 @@
 int main(void)
 
 {
-        int d1, d2, d3;
+	int d1, d2, d3;
 
-        for (d1 = '0'; d1 <= '9'; d1++)
-        {
+	for (d1 = '0'; d1 <= '9'; d1++)
+	{
 
-        for (d2 = d1 + 1; d2 <= '9'; d2++)
-        {
+	for (d2 = d1 + 1; d2 <= '9'; d2++)
+	{
 	for (d3 = d2 + 1; d3 <= '9'; d3++)
 	
-        {
-        putchar(d1);
-        putchar(d2);
+	{
+	putchar(d1);
+	putchar(d2);
 	putchar(d3);
 	if (!(d1 == '7' && d2 == '8' && d3 == '9'))
 	{
-        putchar(',');
-        putchar(' ');
-        }
+	putchar(',');
+	putchar(' ');
 	}
-        }
-        }
-        putchar('\n');
-        return (0);
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
 }
