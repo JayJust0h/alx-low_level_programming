590c9e09312850bf037e4a24668eddae1d839c33
JayJust0h <edwinjustus11@gmail.com>
2023-08-11 12:08:26 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,10 +10,10 @@ int main(void)
 {
 	int d1, d2;
 
-	for (d1 = '0'; d1 < '9'; d1++)
+	for (d1 = '0'; d1 <= '9'; d1++)
 	{
 
-	for (d2 = d1 + 1; d2 '9'; d2++)
+	for (d2 = d1 + 1; d2<= '9'; d2++)
 	{
 	if (d1 != d2)
 	{
@@ -26,6 +26,6 @@ int main(void)
 	}
 	}
 	}
-	putchar('\n);
+	putchar('\n');
 	return (0);
 }
