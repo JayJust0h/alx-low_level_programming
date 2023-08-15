a5cfdfba85cd4caf8b45ce002187906316237627
JayJust0h <edwinjustus11@gmail.com>
2023-08-15 10:31:58 -0700

    5-Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,15 +11,13 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
-		return (1);
+	_putchar('+');
+	return (1);
 	}
-
-       	else if (n == 0)
-	{	
-	_putchar(0);
+	else if (n == 0)
+	{
+	_putchar('0');
 	return (0);
-
 	}
 	else 
 	{
@@ -27,4 +25,3 @@ int print_sign(int n)
 	return (-1);
 	}
 }
-
