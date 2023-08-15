a50250bf823fd9a85af4b52e30d61d5c7da5b287
JayJust0h <edwinjustus11@gmail.com>
2023-08-15 08:27:55 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,8 +4,8 @@
  * print_sign - function assigns the sign on a number
  * @n: the integer being checked
  * Return: 1 where n greater than 0 print +
- * Return: 0 where n equals to 0 print 0 
- * Return: -1 where n less than 0 print -
+ * 0 where n equals to 0 print 0 
+ * -1 where n less than 0 print -
  */
 int print_sign(int n)
 {
