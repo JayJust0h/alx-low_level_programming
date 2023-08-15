5433ec79a259f6b04c41d7606a409c1084bd9065
JayJust0h <edwinjustus11@gmail.com>
2023-08-15 15:52:29 -0700

    Fibinachi

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 1;      
+	int b = 2;      
+	int c;          
+	int sum = 2;
+
+	while ((c = a + b) <= 4000000)
+	{
+	if (c % 2 == 0)
+	{
+	sum += c;
+	}
+
+	a = b;
+	b = c;
+	}
+
+	printf("Sum %d\n", sum);
+
+	return 0;
+}
