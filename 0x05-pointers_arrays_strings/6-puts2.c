57e36512971a9a67a7d3585a455213e4af8c311f
JayJust0h <edwinjustus11@gmail.com>
2023-08-21 17:37:14 +0300

    6-Puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - Print alternating string characters.
+ * new line.
+ * @str: characters to be printed.
+ * Return: character output.
+ */
+
+void puts2(char *str)
+{
+	int length = 0;
+	int i;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	for ( i = 0; i < length; i += 2)
+	{
+		_putchar(str[i]);
+	}
+		_putchar('\n');
+}
