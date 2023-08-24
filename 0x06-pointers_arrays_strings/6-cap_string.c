4d9ef787ce3293b87ffb9413ca7722a972edf8e4
JayJust0h <edwinjustus11@gmail.com>
2023-08-24 10:15:03 -0700

    6-cap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,12 +13,9 @@ char *cap_string(char *str)
 
 	while (str[i])
 	{
-		while (str[i] && !(str[i] >= 'a' && str[i] <= 'z'))
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
 			i++;
-	}
-	if (str[i])
-	{
-		if (i == 0 || str[i - 1] == ' ' ||
+		if (str[i - 1] == ' ' ||
 		str[i - 1] == '\t' ||
 		str[i - 1] == '\n' ||
 		str[i - 1] == ',' ||
@@ -29,11 +26,10 @@ char *cap_string(char *str)
 		str[i - 1] == '"' ||
 		str[i - 1] == '(' ||
 		str[i - 1] == '{' ||
-		str[i - 1] == '}')
-		{
+		str[i - 1] == '}' ||
+		i == 0)
 		str[i] -= 32;
-		}
 		i++;
-	}
+		}
 	return (str);
 }
