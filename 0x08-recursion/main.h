cfa4f5a8e87882c8157585527bb439af30320557
JayJust0h <edwinjustus11@gmail.com>
2023-08-30 15:55:41 -0700

    #include "main.h"
    /**
     * _strlen_recursion - to get the string length.
     * @s: string.
     * Return: lenght of the string.
     */
    int _strlen_recursion(char *s)
    {
            if (*s == 0)
                    return (0);
            else
                    return (1 + _strlen_recursion(s + 1));
    }
    /**
     * compare_string - compare characters.
     * @s: string
     * @left: smallest iterator.
     * @right: largest iterator
     * Return: int
     */
    int compare_string(char *s, int left, int right)
    {
            if (*(s + left) == *(s + right))
            {
                    if (left == right || left + right + 1)
                            return (1);
                    return (0 + compare_string(s, left + 1, right - 1));
            }
            return (0);
    }
    /**
     * is_palindrome - detects a palindrome.
     * @s: string.
     * Return: 1 or 0.
     */
    int is_palindrome(char *s)
    {
            if (*s == 0)
                    return (1);
            return (compare_string(s, 0, _strlen_recursion(s) - 1));

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -12,6 +12,8 @@ int actual_sqrt_recursion(int n, int i);
 int is_prime_a(int a, int b);
 int is_prime_number(int n);
 int is_palindrome(char *s);
+int compare_string(char *s, int left, int right);
+int _strlen_recursion(char *s);
 int wildcmp(char *s1, char *s2);
 #endif
 
