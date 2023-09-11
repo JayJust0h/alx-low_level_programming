960dde7ad059597dda8fd71e13d98e653e044258
JayJust0h <edwinjustus11@gmail.com>
2023-09-11 06:12:56 -0700

    Readme

---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,11 @@
+#ifndef main_h
+#define main_h
+
+
+
+Prototype: void init_dog(struct dog *d, char *name, float age, char *owner);
+Prototype: void print_dog(struct dog *d);
+Prototype: dog_t *new_dog(char *name, float age, char *owner);
+Prototype: void free_dog(dog_t *d);
+
+
