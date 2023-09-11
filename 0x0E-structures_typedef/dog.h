b7550cab1fb4065a487bc230d0a12ec85204ffbf
JayJust0h <edwinjustus11@gmail.com>
2023-09-11 06:32:58 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+/**
+ * struct dog - structure name.
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: name of the owner.
+ * Return: 0 (success).
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -3,9 +3,9 @@
 
 
 
-Prototype: void init_dog(struct dog *d, char *name, float age, char *owner);
-Prototype: void print_dog(struct dog *d);
-Prototype: dog_t *new_dog(char *name, float age, char *owner);
-Prototype: void free_dog(dog_t *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
-
+#endif
