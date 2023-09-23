318710780cd241f22649f0e396546d740abc9f2c
JayJust0h <edwinjustus11@gmail.com>
2023-09-23 10:51:48 -0700

    corrections

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -14,6 +14,13 @@
  * Return: 0 (success)
  */
 
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
 size_t print_list(const list_t *h);
 size_t list_length(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
